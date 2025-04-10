0e6679934be410fe16c3876eaa9dd1e117dbae68
Abhinavv007 <abhinavkumarmishra2009@gmail.com>
2025-04-10 11:25:57 +0530

    update

--- PYTHON/Patterns/hill_prtn.py
+++ PYTHON/Patterns/hill_prtn.py
@@ -1,4 +1,10 @@
 for i in range(5):
     for j in range(i,5):
         print(" ",end=" ")
+    for k in range(i):
+        print("*", end = ' ')
+    for l in range(i+1):
+        print("*", end = ' ')
+    print()
+
     
